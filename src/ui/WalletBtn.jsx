e594048fbd0924e7db7f718d1050a39e05b23784
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-25 17:22:22 -0600

    first commit

--- src/ui/WalletBtn.jsx
+++ src/ui/WalletBtn.jsx
@@ -1,10 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 function WalletBtn({ walletLogo, name }) {
   return (
-    <button className="text-blackLight flex h-[65px] w-full items-center rounded-[2px] border-[0.5px] border-solid border-black pl-[19px] text-xl font-normal leading-[25px]">
-      <img src={walletLogo} className="mr-[43px]" alt="" />
-      Sign in with {name}
-    </button>
+    <NavLink to="/create">
+      <button className="text-blackLight flex h-[65px] w-full items-center rounded-[2px] border-[0.5px] border-solid border-black pl-[19px] text-xl font-normal leading-[25px]">
+        <img src={walletLogo} className="mr-[43px]" alt="" />
+        Sign in with {name}
+      </button>
+    </NavLink>
   );
 }
 
