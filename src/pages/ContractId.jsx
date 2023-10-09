5a41783cb502f1be187fd956ed48fc4f77b1a494
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-08 20:15:10 -0600

    updaTE

---
+++ src/pages/ContractId.jsx
@@ -0,0 +1,22 @@
+function ContractId() {
+  return (
+    <div className="h-[440px]">
+      <h2 className="mb-[17px] text-lg font-semibold leading-default text-black">
+        NDA Contract
+      </h2>
+      <div className="flex h-full justify-center bg-secondaryColor pt-[59px]">
+        <div className="flex h-fit w-[508px] flex-col bg-white px-[36px] pb-[56px] pt-[17px] shadow-primaryShadow">
+          <h2 className="mb-[21px] text-center text-base font-bold leading-default text-black">
+            Transcation ID
+          </h2>
+          <h1 className="text-base font-[800] text-center leading-default text-black mb-4">
+            85940-4034749030-30398
+          </h1>
+          <p className="text-black font-normal text-sm text-center">Copy</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContractId;
